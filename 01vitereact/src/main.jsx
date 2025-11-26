89713489eb9a46445f178d8e80e43a6204fa1c5d
Abinash6000 <abinashdash6000@gmail.com>
2025-11-26 14:54:38 +0530

    basics

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+    retrun (
+        <div>
+            <h1>Custom App !</h1>
+        </div>
+    )
+}
+
 createRoot(document.getElementById('root')).render(
-    <App />
+    <MyApp />
 )
